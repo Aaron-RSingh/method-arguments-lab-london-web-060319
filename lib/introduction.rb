78410937d44063b7f3a6eda6c34c252e5d674b75
Aaron Ramharacksingh <aaron.ramharacksingh@gmail.com>
2019-05-21 16:08:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,5 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
+def introcution_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
